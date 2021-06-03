3474b1e9b075c8f7f612bab33160f0532705ddd7
Oscar <oscareascencioalva@gmail.com>
2021-06-03 15:18:32 -0500

    Primer Commit

---
+++ sources/js/main.js
@@ -0,0 +1,14 @@
+const lista = document.querySelector(".personajes")
+const url = "https://swapi.dev/api/people";
+fetch(url)
+    .then(datos => datos.json())
+    .then(info => mostrarDatos(info.results))
+
+function mostrarDatos(info) {
+    info.forEach(persona => {
+        const { name } = persona;
+        const p = document.createElement("P");
+        p.textContent = name
+        lista.appendChild(p);
+    });
+}
